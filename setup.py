c8ce479d22083afefe8b91988ad3ca6865a2d9e2
B-flexen <62309165+B-flexen@users.noreply.github.com>
2020-10-30 21:03:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Important-Places", # Replace with your own username
+    name="ImportantPlaces", # Replace with your own username
     version="0.0.1",
     author="Benedict Flexen",
     author_email="benedict.flexen@gmail.com",
