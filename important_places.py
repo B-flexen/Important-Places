2aaa9c28d0076284d1a02c837246cb2eaf391d2e
B-flexen <62309165+B-flexen@users.noreply.github.com>
2020-11-02 00:41:04 +0000

    Update important_places.py

--- important_places.py
+++ important_places.py
@@ -50,11 +50,16 @@ intro_label.grid(column = 0, row = 0, padx = 20, pady = 20, columnspan=2)
 
 #Tab 2 - Colne Valley page
 
-marsden_text = """I grew up in the Colne Valley in West Yorkshire on the edge of the Pennines. It a beutiful landscape made only more wonderful by its dynamic nature. I love the changing of the seasons; The colour of the heather on the moors, seeing the hills on a clear day, or only being able to see the bottom in low cloud. I even quite like the rain. But I also notice the changes in the weather year on year. Climate change is becoming more tangable. The dangers it will bring ever more worrying.
-The image to the top right shows the colour scheme based on the current weather conditions in the colne valley and is updated every 20 seconds. As the weather fluctuates only slightly over such short timescales the colour changes are likely to be difficult to percieve. The top lef colour is based on the wind, with the hue being dictated by the wind direction and the saturation being dictaded by the speed. The top centre depends on the elevation of the sun with the colourmap roughly corrisponding the usual colour of the sky. The top right is a greyscale corrisponding to cloudcover; white is 0% cloudcover and dark grey is 100% cloud cover. The bottom left is controlled by the temperature, the bottom middle is rainfall and the bottom right is controlled by the season. The seasons are represented by one of four colours: White for winter, representing the frost and snow, green for spring representing the new growth of plants, a warm yellow-orrange to represent the summer and purple representing the colour of the heather on the moors in early autumn.
+marsden_text = """I grew up in the Colne Valley in West Yorkshire on the edge of the Pennines. It a beutiful landscape made only more wonderful by its dynamic nature. I love the changing of the seasons; The colour of the heather on the moors, seeing the hills on a clear day, or only being able to see the bottom in low cloud. I even quite like the rain. But I also notice the changes in the weather year on year. Climate change is becoming more tangible. The dangers it will bring ever more worrying.
+The image to the top right shows the colour scheme based on the current weather conditions in the colne valley and is updated every 20 seconds. As the weather fluctuates only slightly over such short timescales the colour changes are likely to be difficult to perceive. Bellow is an explanation of each of the different colours.
+Top left: Wind. The hue is dictated by the wind direction and the saturation being dictated by the speed.
+Top centre: Solar elevation. depends on the elevation of the sun with the colourmap roughly corresponding the usual colour of the sky.
+Top right: Cloud cover. A grey scale with white corresponding to 0% cloud cover and dark grey to 100% cloud cover.
+Bottom left: Temperature. A diverging colour scale with the coolest temperatures being represented by blue and the warmest being represented by red.
+Bottom centre: Rainfall. A scale from white to dark blue, with white meaning no rainfall and the darkest blue representing heavy rain, i.e. more than 4mm in the previous hour.
+Bottom right: The seasons. This is one of four colours; White for winter, representing the frost and snow, green for spring representing the new growth of plants, a warm orange to represent the summer and purple representing the colour of the heather on the moors in early autumn.
 
-The video on the bottom right shows the colour scheme based on the weather for the
-same place over all of 2019. Each frame is a 1 hour timestep. """
+The video shows the colour scheme based on the weather for the same place over all of 2019. Each frame is a 1 hour timestep and the video plays at 24fps; you see 1 day per second."""
 
 moors_text_lab = ttk.Label(tab2, text = marsden_text, wraplength=500, justify="left")
 moors_text_lab.grid(column = 0, row = 0, padx = 20, pady = 20)
@@ -88,8 +93,10 @@ video_2019.grid(column = 1, row = 1)
 
 #Tab 3 - Mars page
 
-mars_text = """I have always been fascinated by space: the wonder of the imense unknown and the sheer beauty the small amount we can actually witness. This fascination led me to study astrophysics at university and continues to fuel my desire to work in the space sector. A space is a very big place, I have decided to focus on mars.
-The graph to the right shows the maximum, minimum and average atmospheric temperatures on the surface of mars for each of the past 7 sols (martian days) as measured by the InSight lander. The dataset may ocassionally miss data points for some sols, in which case the graph will only include the available data points. The image in the bottom right is the most resent photgraph taken by Curiosity rover that is available via NASA open data API. """
+mars_text = """I have always been fascinated by space; the wonder of the immense unknown and the sheer beauty of the small amount we can actually witness. This fascination led me to study astrophysics at university and continues to fuel my desire to work in the space sector. As space is a very big place, I have decided to focus just on mars for this application.
+The graph to the right shows the maximum, minimum and average atmospheric temperatures on the surface of mars for each of the past 7 sols (martian days) as measured by the InSight lander. The dataset may occasionally miss data points for some sols, in which case the graph will only include the available data points.
+The image in the bottom right is the most resent photograph taken by Curiosity rover that is available via NASA open data API.
+"""
 
 mars_text_lab = ttk.Label(tab3, text = mars_text, wraplength=500, justify="center")
 mars_text_lab.grid(column = 0, row = 0, padx = 20, pady = 20)
@@ -119,10 +126,9 @@ mars_photo_lab.grid(column = 1, row = 1)
 
 #Tab 4
 
-hull_text = """For the last four years, I have lived and studied in the city
-of Hull. The city is situated on the Humber estuary and has been shaped by its conection to the sea. It has historically been central to the UK fishing industry, it continues to be a significant freight and ferry port. The city centre is shaped by its maritime history, and has multiple quaeys in the city centre, even one of the main shopping centres is built on stilts above an old marina. One of my personal favourite places in the whole city is on the estury bank, with the boats in the marina on one side and the wide estury on the other.
-
-This tab is still a work in progress. Eventually it will display real time tidal data, refelecting the maritime history of the city and the importance that the Humber Estuary and Hull river still has for the city today."""
+hull_text = """For the last four years, I have lived and studied in the city of Hull. The city is situated on the Humber estuary and has been shaped by its connection to the sea. It has historically been central to the UK fishing industry and continues to be a significant freight and ferry port. The city centre is shaped by its maritime history; it has multiple quays in the city centre, even one of the main shopping centres is built on stilts above an old marina. One of my personal favourite places in the whole city is on the estuary bank, with the boats in the marina on one side and the wide estuary on the other.
+This tab is still a work in progress. Eventually it will display real time tidal data, reflecting the maritime history of Hull and the importance that the Humber Estuary and Hull river still has for the city today.
+"""
 
 hull_lab = ttk.Label(tab4, text = hull_text, wraplength=700, justify="center")
 hull_lab.grid(column = 0, row = 0, padx = 20, pady = 20, columnspan=2) 
